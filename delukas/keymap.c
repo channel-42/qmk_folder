4ea307457d999df01e5007df92ed164cc7b50994
channel-42 <59137829+channel-42@users.noreply.github.com>
2020-12-07 16:02:39 +0100

    fixing arrow keys on second layer

--- delukas/keymap.c
+++ delukas/keymap.c
@@ -58,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_LOWER] = LAYOUT( \
   _______, _______, _______, _______, _______, _______,                   _______, _______, _______,_______, _______, _______,\
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, \
-  KC_GRV, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                    KC_LEFT, KC_UP, KC_DOWN, KC_RIGHT, DE_HASH, KC_TILD, \
+  KC_GRV, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                    KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, DE_HASH, KC_TILD, \
   _______, _______, _______, _______, _______, _______, _______, DE_LBRC, XXXXXXX, DE_TILD, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, \
                              _______, _______, _______, _______, _______,  _______, _______, _______\
 ),
